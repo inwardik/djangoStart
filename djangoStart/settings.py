ceb877ef55ad226eb477997e83dda9b6c4b0c536
inwardik <volodymyr.syrotkin@gmail.com>
2021-07-02 12:22:30 +0300

    404 added

--- djangoStart/settings.py
+++ djangoStart/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "django-insecure-*-y*wy%o!4_o=d)+u-ja88wn+8y3zq3c46o9w1h4h&g^y@5wkn"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
